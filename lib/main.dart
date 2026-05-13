44f9bee9abf54da7f7647b349f1e6fdaba4b1979
ahmed <am7328953@gmail.com>
2026-05-13 08:45:20 +0300

    adding equatable.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:demo_project/controllers/bloc/counter_bloc.dart';
+import 'package:demo_project/controllers/cubit/task_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,68 +12,89 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Todo Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Todo Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
+  MyHomePage({super.key, required this.title});
 
   final String title;
+  final TextEditingController controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    print('build parent');
     return BlocProvider(
-      create: (context) => CounterBloc(),
+      create: (context) => TaskCubit(),
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          backgroundColor: Theme
+              .of(context)
+              .colorScheme
+              .inversePrimary,
           title: Text(title),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              const Text('You have pushed the button this many times:'),
-              BlocBuilder<CounterBloc, CounterState>(
-                builder: (context, state) {
-                  return Text(
-                    state.count.toString(),
-                    style: Theme.of(context).textTheme.headlineMedium,
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: BlocBuilder<CounterBloc, CounterState>(
+        body: BlocBuilder<TaskCubit, TaskState>(
           builder: (context, state) {
-            return Column(
-              spacing: 10,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                FloatingActionButton(
-                  onPressed:
-                      () => context.read<CounterBloc>().add(IncrementEvent()),
-                  tooltip: 'Increment',
-                  child: const Icon(Icons.add),
+            final controllerCubit = context.read<TaskCubit>();
+            return GestureDetector(
+              onTap: () => FocusScope.of(context).unfocus(),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: controller,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          focusedBorder: OutlineInputBorder(),
+                          labelText: 'Add task'
+                      ),
+                    ),
+
+                    ElevatedButton(onPressed: () {
+                      if (controller.text.isEmpty) return;
+                      controllerCubit.addTask(title: controller.text);
+                      controller.clear();
+                    }, child: Text('Add')),
+
+                    SizedBox(height: 15,),
+
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: state.tasksList.length,
+                        padding: EdgeInsets.symmetric(horizontal: 15),
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            shape: OutlineInputBorder(),
+                            tileColor: Colors.black54,
+                            title: Text(state.tasksList[index].title,
+                              style: TextStyle(color: Colors.white),),
+                            leading: Checkbox(
+                              activeColor: Colors.green,
+                              value: state.tasksList[index].isCompleted,
+                              onChanged: (value) => controllerCubit.toggleTask(
+                                  state.tasksList[index].id),
+                            ),
+                            trailing: IconButton(onPressed: () =>
+                                controllerCubit.removeTask(
+                                    state.tasksList[index].id),
+                                icon: Icon(Icons.delete, color: Colors.red,)),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
                 ),
-                FloatingActionButton(
-                  onPressed:
-                      () => context.read<CounterBloc>().add(DecrementEvent()),
-                  tooltip: 'decrement',
-                  child: const Icon(Icons.minimize),
-                ),
-              ],
+              ),
             );
           },
-        ), // This trailing comma makes auto-formatting nicer for build methods.
+        ),
       ),
     );
   }
---
+++ lib/models/task_model.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+
+class TaskModel extends Equatable {
+  final String id;
+  final String title;
+  final bool isCompleted;
+
+  const TaskModel({
+    required this.id,
+    required this.title,
+    required this.isCompleted,
+  });
+
+  @override
+  List<Object?> get props => [id, title, isCompleted];
+
+  /// to make new instance to modify id for the toggle function
+  TaskModel copyWith({
+    final String? id,
+    final String? title,
+    final bool? isCompleted,
+  }) {
+    return TaskModel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
+}
